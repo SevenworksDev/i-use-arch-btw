4c60a7001dbfee66422f44e78f1e637ad02205fb
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-18 18:05:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ password = "arch-btw99"
 accountID = "23725858"
 levelString = "H4sIAAAAAAAAC6WOwWoDMQxEf0hbPJLltVl6SHopvS6B9iRCAqHNL_TjK1uF9pAQQgzW2PLMs86rVIIlY9_iGzBWNXCIDMk2wYohpWSzwaC9VLdXw_cdcTwWbxfj3ROB8N-AsPX8JRD4D3RzFr2KSfdMU65g6LyBUOqiISUkk9c4z9H5ldpllTZuPGoAxsMmE7zGK1IIKC0gFDAxTdxI_KIkheD9luBtrvqvTep-73meO8tJzRdJJ7qTSWuEfVAmyT08K-nsMWHCU3YzaL9uceDd6eP97Wv_sv08vh6el4EmltbGn5N_uvwAZJh_AKECAAA="
 
+# You can upload only 5 levels an hour, decreasing this gets you ratelimited for a while. Python Requests is very slow so you shouldnt have issues.
+# Verified by Kitsune YouTuber nerd himself
+# https://www.reddit.com/r/geometrydash/comments/14x9icw/comment/jrmxe2j/
+
 def xor_cipher(string, key):
     return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(string, itertools.cycle(key)))
 
@@ -35,5 +39,6 @@ while True:
     req = requests.post("http://www.boomlings.com/database/uploadGJLevel21.php", data={"gameVersion": 21,"accountID": accountID, "gjp": gjp_encrypt(password), "userName": username, "levelID": 0,"levelName": f"i use arch btw {str(random.randint(1,10000))}", "levelDesc": "","levelVersion": 1,"levelLength": 0,"audioTrack": 0,"auto": 0,"password": 0,"original": 0,"twoPlayer": 0,"songID": 1,"objects": 0,"coins": 0,"requestedStars": 0, "unlisted": 0,"ldm": 0,"levelString": encode_level(levelString, False), "seed2": generate_chk(key="41274", values=[generate_upload_seed(levelString)], salt="xI25fpAapCQg"), "secret": "Wmfd2893gb7"}, headers={"User-Agent":""}).text
     if req == "-1":
         print("Failure due to either invalid login, creator banned or skill issue.")
-    elif
+    elif:
         print("Success!")
+    time.sleep(720)
